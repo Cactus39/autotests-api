9701dc9d5708646c8ea200b31b40c8e316a6a1c1
Cactus39 <daniilcactus@gmail.com>
2026-02-26 14:15:29 +0000

    Практикуемся в написании API автотестов. Часть 3. Практика написания API автотестов для работы с заданиями. Создание задания.

---
+++ tests/exercises/test_exercises.py
@@ -0,0 +1,28 @@
+import pytest
+from http import HTTPStatus
+from clients.exercises.exercises_schema import CreateExerciseRequestSchema, CreateExerciseResponseSchema
+from clients.exercises.exercises_client import ExercisesClient
+from fixtures.courses import CourseFixture
+from tools.assertions.exercises import assert_create_exercise_response
+from tools.assertions.base import assert_status_code
+from tools.assertions.schema import validate_json_schema
+
+@pytest.mark.regression
+@pytest.mark.exercises
+class TestExercises:
+    @pytest.mark.current
+    def test_create_exercise(self, function_course: CourseFixture, exercises_client: ExercisesClient):
+
+        # Формируем параметры запроса
+        request = CreateExerciseRequestSchema()
+        # Отправляем запрос на создание задания
+        response = exercises_client.create_exercise_api(request=request)
+        # Проверяем, что код ответа 200 OK
+        assert_status_code(response.status_code, HTTPStatus.OK)
+
+        # Десериализуем JSON-ответ в Pydantic-модель
+        response_data = CreateExerciseResponseSchema.model_validate_json(response.text)
+        # Проверяем, что данные в ответе соответствуют запросу
+        assert_create_exercise_response(response_data, request)
+        # Проверяем соответствие JSON-ответа схеме
+        validate_json_schema(response.json(), response_data.model_json_schema())
