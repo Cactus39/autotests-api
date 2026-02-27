e8564d57c47879bf6a186d44ab5b21e4dcfce1e1
Cactus39 <daniilcactus@gmail.com>
2026-02-27 19:45:30 +0000

    Практикуемся в написании API автотестов. Часть 3. Практика написания API автотестов для работы с заданиями. Удаление задания.

--- tests/exercises/test_exercises.py
+++ tests/exercises/test_exercises.py
@@ -1,12 +1,14 @@
 import pytest
 from http import HTTPStatus
+
+from clients.errors_schema import ValidationErrorSchema, InternalErrorResponseSchema
 from clients.exercises.exercises_schema import CreateExerciseRequestSchema, CreateExerciseResponseSchema, \
     GetExerciseResponseSchema, UpdateExerciseRequestSchema, UpdateExerciseResponseSchema
 from clients.exercises.exercises_client import ExercisesClient
 from fixtures.courses import CourseFixture
 from fixtures.exercises import ExerciseFixture
 from tools.assertions.exercises import assert_create_exercise_response, assert_get_exercise_response, \
-    assert_update_exercise_response
+    assert_update_exercise_response, assert_exercise_not_found_response
 from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
 
@@ -44,7 +46,6 @@ class TestExercises:
         # Проверяем соответствие JSON-ответа схеме
         validate_json_schema(response.json(), response_data.model_json_schema())
 
-    # @pytest.mark.current
     def test_update_exercise(self, function_exercise: ExerciseFixture, exercises_client: ExercisesClient):
         # Формируем параметры запроса
         exercise_id = function_exercise.response.exercise.id
@@ -60,3 +61,23 @@ class TestExercises:
         assert_update_exercise_response(response_data, update_request)
         # Валидируем JSON-схему ответа
         validate_json_schema(response.json(), response_data.model_json_schema())
+
+    # @pytest.mark.current
+    def test_delete_exercise(self, function_exercise: ExerciseFixture, exercises_client: ExercisesClient):
+        exercise_id = function_exercise.response.exercise.id
+        # Удаляем задание
+        delete_response = exercises_client.delete_exercise_api(exercise_id=exercise_id)
+        # Проверяем, что задание успешно удалено (статус 200 OK)
+        assert_status_code(delete_response.status_code, HTTPStatus.OK)
+
+        # Пытаемся получить удаленное задание
+        get_response = exercises_client.get_exercise_api(exercise_id=exercise_id)
+        # Проверяем, что сервер вернул 404 Not Found
+        assert_status_code(get_response.status_code, HTTPStatus.NOT_FOUND)
+
+        # Проверяем, что в ответе содержится ошибка "Exercise not found"
+        response_data = InternalErrorResponseSchema.model_validate_json(get_response.text)
+        assert_exercise_not_found_response(response_data)
+
+        # Проверяем, что ответ соответствует схеме
+        validate_json_schema(get_response.json(), response_data.model_json_schema())
