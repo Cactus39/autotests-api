3baf1f0064688c9e7efb4fec0a5a782506267385
Cactus39 <daniilcactus@gmail.com>
2026-02-27 21:04:04 +0000

    Практикуемся в написании API автотестов. Часть 3. Практика написания API автотестов для работы с заданиями. Получение списка заданий.

--- tests/exercises/test_exercises.py
+++ tests/exercises/test_exercises.py
@@ -3,12 +3,13 @@ from http import HTTPStatus
 
 from clients.errors_schema import ValidationErrorSchema, InternalErrorResponseSchema
 from clients.exercises.exercises_schema import CreateExerciseRequestSchema, CreateExerciseResponseSchema, \
-    GetExerciseResponseSchema, UpdateExerciseRequestSchema, UpdateExerciseResponseSchema
+    GetExerciseResponseSchema, UpdateExerciseRequestSchema, UpdateExerciseResponseSchema, GetExercisesQuerySchema, \
+    GetExercisesResponseSchema
 from clients.exercises.exercises_client import ExercisesClient
 from fixtures.courses import CourseFixture
 from fixtures.exercises import ExerciseFixture
 from tools.assertions.exercises import assert_create_exercise_response, assert_get_exercise_response, \
-    assert_update_exercise_response, assert_exercise_not_found_response
+    assert_update_exercise_response, assert_exercise_not_found_response, assert_get_exercises_response
 from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
 
@@ -62,7 +63,6 @@ class TestExercises:
         # Валидируем JSON-схему ответа
         validate_json_schema(response.json(), response_data.model_json_schema())
 
-    # @pytest.mark.current
     def test_delete_exercise(self, function_exercise: ExerciseFixture, exercises_client: ExercisesClient):
         exercise_id = function_exercise.response.exercise.id
         # Удаляем задание
@@ -81,3 +81,19 @@ class TestExercises:
 
         # Проверяем, что ответ соответствует схеме
         validate_json_schema(get_response.json(), response_data.model_json_schema())
+
+    def test_get_exercises(self, function_exercise: ExerciseFixture, exercises_client: ExercisesClient, function_course: CourseFixture):
+        # Формируем параметры запроса, передавая course_id
+        query = GetExercisesQuerySchema(course_id=function_course.response.course.id)
+        # Отправляем GET-запрос на получение списка заданий
+        response = exercises_client.get_exercises_api(query=query)
+        # Проверяем, что код ответа 200 OK
+        assert_status_code(response.status_code, HTTPStatus.OK)
+
+        # Десериализуем JSON-ответ в Pydantic-модель
+        response_data = GetExercisesResponseSchema.model_validate_json(response.text)
+        # Проверяем, что список заданий соответствует ранее созданным заданиям
+        assert_get_exercises_response(response_data, [function_exercise.response])
+        # Проверяем соответствие JSON-ответа схеме
+        validate_json_schema(response.json(), response_data.model_json_schema())
+
